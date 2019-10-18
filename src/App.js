a41c650048995fb46c4876a33580343735e777be
Rostyslav Yurchyk <ros_ur@ukr.net>
2019-10-18 22:29:02 +0200

    add redux binding for react

--- src/App.js
+++ src/App.js
@@ -1,104 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import './App.css';
 import Post from './Post';
 import data from './data.js';
 
-// const posts = (state = [], action) => {
-//     if (action.type === 'UPVOTE') {
-//         return state.map(p => {
-//             if (p.id !== action.id) return p;
-//             return {
-//                 id: p.id,
-//                 title: p.title,
-//                 author: p.author,
-//                 comments: p.comments,
-//                 rating: p.rating + 1,
-//             };
-//         });
-//     } else if (action.type === 'DOWNVOTE') {
-//         return state.map(p => {
-//             if (p.id !== action.id) return p;
-//             return {
-//                 id: p.id,
-//                 title: p.title,
-//                 author: p.author,
-//                 comments: p.comments,
-//                 rating: p.rating - 1,
-//             };
-//         });
-//     } else return state;
-// };
+const posts = (state = [], action) => {
+    if (action.type === 'UPVOTE') {
+        return state.map(p => {
+            if (p.id !== action.id) return p;
+            return {
+                id: p.id,
+                title: p.title,
+                author: p.author,
+                comments: p.comments,
+                rating: p.rating + 1,
+            };
+        });
+    } else if (action.type === 'DOWNVOTE') {
+        return state.map(p => {
+            if (p.id !== action.id) return p;
+            return {
+                id: p.id,
+                title: p.title,
+                author: p.author,
+                comments: p.comments,
+                rating: p.rating - 1,
+            };
+        });
+    } else return state;
+};
 
-// const store = createStore(posts);
+const store = createStore(posts);
 
 class App extends React.Component {
     state = { posts: [] };
 
     componentDidMount() {
-        this.setState({ posts: data });
+        // this.setState({ posts: data });
+        // store.dispatch();
     }
 
-    vote = (curPost, userRating) => {
-        const updPosts = this.state.posts.map(p => {
-            if (p.id !== curPost.id) return p;
-            return {
-                id: curPost.id,
-                title: curPost.title,
-                flair: curPost.flair,
-                author: curPost.author,
-                comments: curPost.comments,
-                rating: curPost.rating + userRating,
-            };
-        });
-        this.setState({ posts: updPosts });
-    };
+    // vote = (curPost, userRating) => {
+    //     const updPosts = this.state.posts.map(p => {
+    //         if (p.id !== curPost.id) return p;
+    //         return {
+    //             id: curPost.id,
+    //             title: curPost.title,
+    //             flair: curPost.flair,
+    //             author: curPost.author,
+    //             comments: curPost.comments,
+    //             rating: curPost.rating + userRating,
+    //         };
+    //     });
+    //     this.setState({ posts: updPosts });
+    // };
 
     render() {
         return (
-            <main className="main">
-                {[...this.state.posts] // avoid mutation by sort()
-                    .sort((a, b) => b.rating - a.rating)
-                    .map(post => {
-                        return <Post key={post.id} vote={this.vote} {...post} />;
-                    })}
-            </main>
+            <Provider store={store}>
+                <main className="main">
+                    {[...store.getState()] // avoid mutation by sort()
+                        .sort((a, b) => b.rating - a.rating)
+                        .map(post => {
+                            return <Post key={post.id} vote={this.vote} {...post} />;
+                        })}
+                </main>
+            </Provider>
         );
     }
 }
-
-// function Main(props) {
-//     const { posts, vote } = props;
-//     // const [posts, setPosts] = useState([]);
-
-//     // useEffect(() => {
-//     //     setPosts(data);
-//     // }, []);
-
-//     // const vote = (curPost, userRating) => {
-//     //     const updPosts = posts.map(p => {
-//     //         if (p.id !== curPost.id) return p;
-//     //         return {
-//     //             id: curPost.id,
-//     //             title: curPost.title,
-//     //             flair: curPost.flair,
-//     //             author: curPost.author,
-//     //             comments: curPost.comments,
-//     //             rating: curPost.rating + userRating,
-//     //         };
-//     //     });
-//     //     setPosts(updPosts);
-//     // };
-
-//     return (
-//         <main className="main">
-//             {[...posts] // avoid mutation by sort()
-//                 .sort((a, b) => b.rating - a.rating)
-//                 .map(post => {
-//                     return <Post key={post.id} vote={vote} {...post} />;
-//                 })}
-//         </main>
-//     );
-// }
 
 export default App;
